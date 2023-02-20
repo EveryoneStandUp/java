2123f7a08913338f23a99a3f5e00d2f47a18314e
EveryoneStandUp <wjsgudwns98@naver.com>
2023-02-20 16:52:26 +0900

    java

---
+++ src/ch04/exercise/Example04.java
@@ -0,0 +1,27 @@
+package ch04.exercise;
+
+public class Example04 {
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+
+	    do {
+		num1 = ((int)(Math.random()*6)) + 1;
+		num2 = ((int)(Math.random()*6)) + 1;
+		
+		System.out.printf("(%d, %d)%n", num1, num2);
+	    } while ((num1 + num2) != 5);
+
+	    
+//		while (true) {
+//			num1 = (int) (Math.random() * 6) + 1;
+//			num2 = (int) (Math.random() * 6) + 1;
+//
+//			if ((num1 + num2) == 5) {
+//				System.out.println(" ( " + num1 + " , " + num2 + " ) ");
+//				break;
+//			}
+//		}
+//	}
+}
+}
