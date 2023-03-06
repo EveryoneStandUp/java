c576cde2d6048964e47049a314e03e24f20690a3
EveryoneStandUp <wjsgudwns98@naver.com>
2023-03-06 16:36:32 +0900

    java

---
+++ src/ch07/exercise/exam10/Computer.java
@@ -0,0 +1,19 @@
+package ch07.exercise.exam10;
+
+public class Computer extends Machine {
+	@Override
+	public void powerOn() {
+		
+	}
+	
+	@Override
+	public void powerOff() {
+
+	}
+
+	@Override
+	public void work() {
+		
+	}
+}
+
---
+++ src/ch07/lecture/p04abstract/Fish.java
@@ -0,0 +1,9 @@
+package ch07.lecture.p04abstract;
+
+public class Fish extends Animal {
+	@Override
+	public void breath() {
+		System.out.println("아가미로 숨쉼");
+	}
+
+}
